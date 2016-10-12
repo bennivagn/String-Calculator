6aa60c4abd7e68c3f7313b14078e500aa97cb035
bennivagn <bennivagn@gmail.com>
2016-10-12 22:20:23 +0000

    nú virkar fyrsta test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,7 +3,14 @@ package is.ru.stringcalculator;
 public class Calculator {
 	
 	public static int add(String text){
-		return -1;
+		if (text == "")
+		{
+			return 0;
+		}
+		else
+		{
+			return -1;
+		}
 	}
 
 }
