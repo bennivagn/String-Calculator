cd0bea5f6ff55641e3ad1552979c60b5be7b7a11
bennivagn <bennivagn@gmail.com>
2016-10-14 17:31:00 +0000

    núna kastast IllegalArgumentException ef tala er mínus tala

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,14 +3,17 @@ package is.ru.stringcalculator;
 public class Calculator {
 	
 	
-	public static int add(String text){
+	public static int add(String text) throws Exception
+	{
 		text = filterString(text);
 		String[] numbers = text.split(",");
+		
 		if (text == "")
 		{
 			return 0;
 		}
 		
+		validatenumbers(numbers);
 		if(text.length() == 1)
 		{
 			return Integer.parseInt(text);
@@ -28,7 +31,20 @@ public class Calculator {
 		return text;
 	}
 	
-	private static int sum(String[] stringarray)
+	private static void validatenumbers(String[] numbers) throws Exception
+	{
+		for(int i = 0; i < numbers.length;i++)
+		{
+			if(Integer.parseInt(numbers[i]) < 0)
+			{
+				throw new IllegalArgumentException("Negatives not allowed");
+			}
+
+		}
+
+	}
+	
+	private static int sum(String[] stringarray) throws Exception
 	{
 		int totalsum = 0;
 		for(int i = 0; i < stringarray.length;i++)
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -4,40 +4,48 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest{
+
 	
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString() throws Exception{
 		assertEquals(0, Calculator.add(""));
 	}
 	
 	@Test
-	public void testSumOneNumber() {
+	public void testSumOneNumber() throws Exception{
 		assertEquals(7, Calculator.add("7"));
 	}
 	
 	@Test
-	public void testSumsTwoNumbers() {
+	public void testSumsTwoNumbers() throws Exception{
 		assertEquals(7, Calculator.add("3,4"));
 	}
 	
 	@Test
-	public void testSumOfMany1() {
+	public void testSumOfMany1() throws Exception{
 		assertEquals(25,Calculator.add("10,10,5"));
 	}
 	
 	@Test
-	public void testSumOfMany2() {
+	public void testSumOfMany2() throws Exception{
 		assertEquals(2000,Calculator.add("1000,500,500"));
 	}
 	
 	@Test
-	public void testSumOfMany3() {
+	public void testSumOfMany3() throws Exception{
 		assertEquals(695,Calculator.add("54,23,92,521,5"));
 	}
 	
 	@Test
-	public void addWithNewLine()
+	public void addWithNewLine() throws Exception
 	{
 		assertEquals(6,Calculator.add("1\n2,3"));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void negativeInput() throws Exception
+	{
+		
+		Calculator.add("4,1,10,-542");
 	}
 }
